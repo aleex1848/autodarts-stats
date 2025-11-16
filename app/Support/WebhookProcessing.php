61d00ae63a6e542c5cc5a1eaf8b250d52f0537d2
aleex1848 <halycon01@gmail.com>
2025-11-16 17:57:40 +0000

    fix webhook procesing [restart]

--- app/Support/WebhookProcessing.php
+++ app/Support/WebhookProcessing.php
@@ -17,5 +17,36 @@ class WebhookProcessing extends ProcessWebhookJob
             ]);            
         }
 
-    
+        public function handle()
+        {
+            $event = $this->webhookCall->payload['event'];
+            Log::debug("event received: $event"); // contains an instance of `WebhookCall`
+            switch ($event) {
+                case 'throw':
+                    $this->handleThrow();
+                    break;
+                case 'match_state':
+                    $this->handleMatchState();
+                    break;
+            }
+        }
+
+    public function handleThrow()
+    {
+        Log::debug("throw received",[
+            'matchId' => $this->webhookCall->payload['matchId'],
+            'player' => $this->webhookCall->payload['data']['playerName'],
+            'player_id' => $this->webhookCall->payload['data']['playerId'],
+            'hit' => $this->webhookCall->payload['data']['throw']['segment']['name'],
+            'multiplier' => $this->webhookCall->payload['data']['throw']['segment']['multiplier'],
+            'number' => $this->webhookCall->payload['data']['throw']['segment']['number'],
+        ]);
+    }
+
+    public function handleMatchState()
+    {
+        Log::debug("match_state",[
+            'matchId' => $this->webhookCall->payload['matchId'],
+        ]);
+    }
 }
