f8e7bc3664ee281a22d444f653bd4fa0f7df9ac0
aleex1848 <halycon01@gmail.com>
2025-12-01 11:04:29 +0000

    fix cron supervisor [restart]

--- docker/php-nginx-cron/bin/service.d/cron.d/10-init.sh
+++ docker/php-nginx-cron/bin/service.d/cron.d/10-init.sh
@@ -1,15 +1,23 @@
 # Install crontab files
 if [[ -d "/opt/docker/etc/cron" ]]; then
-    mkdir -p /etc/cron.d/
+    mkdir -p /etc/crontabs/
 
-    find /opt/docker/etc/cron -type f | while read CRONTAB_FILE; do
-        # fix permissions
-        chmod 0644 -- "$CRONTAB_FILE"
+    find /opt/docker/etc/cron -type f -name "root" | while read CRONTAB_FILE; do
+        DEST_FILE="/etc/crontabs/root"
 
-        # add newline, cron needs this
-        echo >> "$CRONTAB_FILE"
+        # Prepend our cron entries to the existing root crontab
+        if [[ -f "$DEST_FILE" ]]; then
+            # Create temp file with our entries + existing entries
+            cat "$CRONTAB_FILE" > "${DEST_FILE}.new"
+            echo "" >> "${DEST_FILE}.new"
+            cat "$DEST_FILE" >> "${DEST_FILE}.new"
+            mv "${DEST_FILE}.new" "$DEST_FILE"
+        else
+            cp -a -- "$CRONTAB_FILE" "$DEST_FILE"
+        fi
 
-        # Install files
-        cp -a -- "$CRONTAB_FILE" "/etc/crontabs/$(basename "$CRONTAB_FILE")"
+        # Fix permissions: BusyBox crond requires 0600 for /etc/crontabs/
+        chmod 0600 -- "$DEST_FILE"
+        chown root:root -- "$DEST_FILE"
     done
 fi
