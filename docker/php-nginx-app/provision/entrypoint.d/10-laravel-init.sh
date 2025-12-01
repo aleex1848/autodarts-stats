2a6e080e99b0e55b26d1386bcbe243136bbf9780
aleex1848 <halycon01@gmail.com>
2025-12-01 12:37:50 +0000

    fix production build [restart]

--- docker/php-nginx-app/provision/entrypoint.d/10-laravel-init.sh
+++ docker/php-nginx-app/provision/entrypoint.d/10-laravel-init.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 cd /app
+
+# Stelle sicher, dass alle Storage-Verzeichnisse existieren, bevor composer install ausgeführt wird
+# (package:discover benötigt diese Verzeichnisse)
+mkdir -p /app/storage/framework/cache/data
+mkdir -p /app/storage/framework/sessions
+mkdir -p /app/storage/framework/views
+mkdir -p /app/storage/framework/testing
+mkdir -p /app/storage/logs
+chmod -R 775 /app/storage
+chown -R 1000:1000 /app/storage
+
 composer install --no-interaction --no-dev --prefer-dist
 #php artisan telescope:install
 #php artisan horizon:publish
