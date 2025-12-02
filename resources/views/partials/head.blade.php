30845ed502b333343cbb0f08680997a21bd7f889
aleex1848 <halycon01@gmail.com>
2025-12-02 12:15:31 +0000

    favicon und social-media-meta-data [restart]

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,9 +3,24 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
+{{-- Favicon --}}
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+{{-- Open Graph Meta Tags für WhatsApp und andere Social Media --}}
+<meta property="og:type" content="website" />
+<meta property="og:title" content="{{ $title ?? 'Autodarts-Liga.de' }}" />
+<meta property="og:description" content="{{ $description ?? 'Autodarts-Liga.de - Die Liga für Autodarts' }}" />
+<meta property="og:url" content="{{ url()->current() }}" />
+<meta property="og:image" content="{{ url('/favicon.svg') }}" />
+<meta property="og:site_name" content="Autodarts-Liga.de" />
+
+{{-- Twitter Card --}}
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:title" content="{{ $title ?? 'Autodarts-Liga.de' }}" />
+<meta name="twitter:description" content="{{ $description ?? 'Autodarts-Liga.de - Die Liga für Autodarts' }}" />
+<meta name="twitter:image" content="{{ url('/favicon.svg') }}" />
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
